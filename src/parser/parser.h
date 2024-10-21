a1046807b0d3c8190a5a404b4c48959dc4896618
nardini <evandro.nardini@gmail.com>
2024-10-21 19:33:24 -0300

    norminette corrections

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 16:26:04 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/12 16:26:04 by marvin           ###   ########.fr       */
+/*   Updated: 2024/10/21 19:29:09 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../shell.h"
+#include "../src/tokenizer/tokenizer.h"
 
-typedef struct s_command {
-    char            **args;          // List of arguments
-    char            *input_file;     // Input file for redirection
-    char            *output_file;    // Output file for redirection
-    int             append;          // Flag for '>>' (1 for append, 0 for overwrite)
-    char            *heredoc_delimiter;  // Delimiter for heredoc '<<'
-    struct s_command *next;          // Next command in the pipeline
-} t_command;
+typedef struct s_command
+{
+	char				**args;          // List of arguments
+	char				*input_file;     // Input file for redirection
+	char				*output_file;    // Output file for redirection
+	int					append;          // Flag for '>>' (1 for append, 0 for overwrite)
+	char				*heredoc_delimiter;  // Delimiter for heredoc '<<'
+	struct s_command	*next;          // Next command in the pipeline
+}	t_command;
