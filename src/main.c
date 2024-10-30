508b3fd2c4d17b533207491a256016ece4b464b5
Seiji Ueno <sueno-te@c2r10p6.42sp.org.br>
2024-10-30 20:51:45 -0300

    WIP: End of the Day. Safeguard

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:14:55 by emorales          #+#    #+#             */
-/*   Updated: 2024/10/30 18:11:33 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/10/30 19:26:58 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	main(void)
 		}
 		print_tokens(tokens);
 		tree = tree_constructor(tokens);
-		print_ast(tree, 2);
+		print_ast(tree, 6);
 		free_tokens(tokens);
 		free(tree); // free_tree to be build!!! must free all of it
 		free(input);
