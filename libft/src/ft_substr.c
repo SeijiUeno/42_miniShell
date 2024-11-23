e03874b1250f98d38233b5fe16361e286f23fa13
Seiji Ueno <sueno-te@c2r12p2.42sp.org.br>
2024-11-22 21:03:42 -0300

    Libft Updated with Garbage Collector

--- libft/src/ft_substr.c
+++ libft/src/ft_substr.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 02:03:33 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/10/04 17:33:31 by emorales         ###   ########.fr       */
+/*   Updated: 2024/11/22 20:05:51 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include "../includes/garbage_collector.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -22,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (len > slen - start)
 		return (ft_strdup(s + start));
-	str = malloc(len + 1);
+	str = gc_allocate(len + 1);
 	if (!str)
 		return (NULL);
 	ft_strlcpy(str, s + start, len + 1);
