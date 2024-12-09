58f6d9b1c8442d950dcfbe86d9eacb9d60537e01
Seiji Ueno <sueno-te@c2r12p2.42sp.org.br>
2024-12-09 16:58:43 -0300

    WIP: header update

--- src/shell.h
+++ src/shell.h
@@ -6,7 +6,7 @@
 /*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:05:19 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/01 21:20:34 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:58:04 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,10 @@
 # define STATUS_SIGINT 130
 # define STATUS_SIGQUIT 131
 
+# define WHITESPACE " \t\n\r\v\f"
+# define SYMBOLS "|<>"
+# define QUOTES "'\""
+
 typedef struct s_minishell	t_minishell;
 
 struct s_minishell
@@ -47,5 +51,39 @@ struct s_minishell
 	t_token		*tokens;
 	t_command	*tree_cmd;
 };
+
+struct s_token
+{
+	int				type;
+	char			*content;
+	struct s_token	*next;
+	struct s_token	*prev;
+};
+
+enum e_operator_type
+{
+	PIPE = 1,
+	REDIR_IN,
+	REDIR_OUT,
+	APPEND,
+	HEREDOC,
+	WORD,
+	OPERATOR,
+};
+
+// Tokenizer Functions
+void generate_tokens(char *input, t_token **tokens);
+void assign_operator_token_types(t_token **tokens);
+void initialize_token_list(t_token **tokens);
+void append_token(t_token **token_list, t_token *new_token);
+void free_all_tokens(t_token **tokens);
+
+// Token Validation Functions
+int validate_tokens(t_token *tokens);
+int check_leading_token(t_token *tokens);
+int check_trailing_token(t_token *tokens);
+int check_token_sequence(t_token *tokens);
+int is_redirection(int token_type);
+void append_redirection(t_token **redirections, t_token *redir, t_token *redir_target, t_minishell *minishell);
 
 #endif
