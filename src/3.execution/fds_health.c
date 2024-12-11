834846d5b5aa7d81803de06f3a9864c5c27b2615
Seiji Ueno <sueno-te@c1r7p1.42sp.org.br>
2024-12-11 20:07:34 -0300

    redir

---
+++ src/3.execution/fds_health.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fds_health.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 17:16:52 by sueno-te          #+#    #+#             */
+/*   Updated: 2024/12/11 19:58:35 by sueno-te         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/shell.h"
+
+void	reset_fds(t_minishell *minishell)
+{
+	if (!minishell)
+	{
+		fprintf(stderr, "minishell: invalid minishell structure\n");
+		return;
+	}
+
+	// Restore standard input and output
+	if ((dup2(minishell->stdin_backup, STDIN_FILENO) < 0)
+		|| (dup2(minishell->stdout_backup, STDOUT_FILENO) < 0))
+	{
+		perror("dup2");
+		return;
+	}
+}
