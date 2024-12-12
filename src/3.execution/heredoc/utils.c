24fa5d59f4c02014288b2f7867356adbafa549b9
Seiji Ueno <142241481+SeijiUeno@users.noreply.github.com>
2024-12-12 19:09:47 -0300

    ok

--- src/3.execution/heredoc/utils.c
+++ src/3.execution/heredoc/utils.c
@@ -6,7 +6,7 @@
 /*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 14:22:07 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 14:33:56 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/12 19:09:33 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,14 @@ char	*generate_heredoc_name(int index)
 	if (!full_name)
 		ft_putstr_fd("Error: Failed to generate heredoc name\n", STDERR_FILENO);
 	return (full_name);
+}
+
+void	handle_sig_heredoc(int signal)
+{
+	if (signal == SIGINT)
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		close(STDIN_FILENO);
+		control_status(STATUS_SIGINT);
+	}
 }
