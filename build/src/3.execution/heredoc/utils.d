3157e149cc856a2c2ad49293dad5de4ffbf9a06f
Seiji Ueno <142241481+SeijiUeno@users.noreply.github.com>
2024-12-13 15:52:33 -0300

    signals update

---
+++ build/src/3.execution/heredoc/utils.d
@@ -0,0 +1,10 @@
+build/src/3.execution/heredoc/utils.o: src/3.execution/heredoc/utils.c \
+  src/3.execution/heredoc/../../../includes/shell.h \
+  includes/../libs/libft/includes/libft.h \
+  includes/../libs/garbage_collector/includes/garbage_collector.h
+
+src/3.execution/heredoc/../../../includes/shell.h:
+
+includes/../libs/libft/includes/libft.h:
+
+includes/../libs/garbage_collector/includes/garbage_collector.h:
---
+++ build/src/3.execution/path_handler/path_command.d
@@ -0,0 +1,11 @@
+build/src/3.execution/path_handler/path_command.o: \
+  src/3.execution/path_handler/path_command.c \
+  src/3.execution/path_handler/../../../includes/shell.h \
+  includes/../libs/libft/includes/libft.h \
+  includes/../libs/garbage_collector/includes/garbage_collector.h
+
+src/3.execution/path_handler/../../../includes/shell.h:
+
+includes/../libs/libft/includes/libft.h:
+
+includes/../libs/garbage_collector/includes/garbage_collector.h:
