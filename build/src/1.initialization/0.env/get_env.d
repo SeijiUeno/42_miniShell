dd299693407f2b455f28e828355b67efc59b4adc
Seiji Ueno <sueno-te@c2r12p2.42sp.org.br>
2024-12-13 15:52:33 -0300

    signals update

---
+++ build/src/1.initialization/0.env/get_env.d
@@ -0,0 +1,11 @@
+build/src/1.initialization/0.env/get_env.o: \
+  src/1.initialization/0.env/get_env.c \
+  src/1.initialization/0.env/../../../includes/shell.h \
+  includes/../libs/libft/includes/libft.h \
+  includes/../libs/garbage_collector/includes/garbage_collector.h
+
+src/1.initialization/0.env/../../../includes/shell.h:
+
+includes/../libs/libft/includes/libft.h:
+
+includes/../libs/garbage_collector/includes/garbage_collector.h:
---
+++ build/src/aux/free.d
@@ -0,0 +1,9 @@
+build/src/aux/free.o: src/aux/free.c includes/../includes/shell.h \
+  includes/../libs/libft/includes/libft.h \
+  includes/../libs/garbage_collector/includes/garbage_collector.h
+
+includes/../includes/shell.h:
+
+includes/../libs/libft/includes/libft.h:
+
+includes/../libs/garbage_collector/includes/garbage_collector.h:
