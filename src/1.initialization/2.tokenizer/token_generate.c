d3ce8d1222894c235400e9785c973994bdde26ac
nardini <evandro.nardini@gmail.com>
2024-12-13 13:13:32 -0300

    clear

--- src/1.initialization/2.tokenizer/token_generate.c
+++ src/1.initialization/2.tokenizer/token_generate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_generate.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:42:46 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/13 11:30:27 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/13 13:10:17 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,14 +77,13 @@ void	add_token(t_token_data *data, int start, int end)
 	*data->current = new_token;
 }
 
-void assign_operator_token_types(t_token **tokens)
+void	assign_operator_token_types(t_token **tokens)
 {
 	t_token		*current;
 	char		op_char;
 
 	if (!tokens || !*tokens)
-	return; // Safeguard against NULL pointers
-
+		return ; // Safeguard against NULL pointers
 	current = *tokens;
 	while (current)
 	{
@@ -96,12 +95,11 @@ void assign_operator_token_types(t_token **tokens)
 			else if (op_char == '<' || op_char == '>')
 				current->type = assign_redir_type(op_char, current->content);
 			else if (current->content[1] != '\0')
-			error("minishell", "syntax error near unexpected token", -1);
+				error("minishell", "syntax error near unexpected token", -1);
 		}
 		current = current->next;
 	}
 }
-
 
 t_token	*tokenize_input(char *input)
 {
