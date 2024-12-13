e5aef011103fe1ed7d2b4132d3cd762bdc0edd3b
nardini <evandro.nardini@gmail.com>
2024-12-13 14:10:40 -0300

    patch main

--- src/1.initialization/1.main/init_shell_env.c
+++ src/1.initialization/1.main/init_shell_env.c
@@ -6,50 +6,53 @@
 /*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:35:51 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/13 13:59:07 by emorales         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:05:07 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/shell.h"
 
-static void initialize_signals(void)
+static void	initialize_signals(void)
 {
-    signal_setup();
+	signal_setup();
 }
 
-static char **initialize_env(void)
+static char	**initialize_env(void)
 {
-    extern char **environ;
-    char **env = get_env(environ);
-    if (!env)
-    {
-        perror("Failed to initialize environment");
-        exit(EXIT_FAILURE);
-    }
-    return env;
+	extern char	**environ;
+	char		**env;
+
+	env = get_env(environ);
+	if (!env)
+	{
+		perror("Failed to initialize environment");
+		exit(EXIT_FAILURE);
+	}
+	return (env);
 }
 
-static int backup_standard_fds(int *stdin_backup, int *stdout_backup)
+static int	backup_standard_fds(int *stdin_backup, int *stdout_backup)
 {
-    *stdin_backup = dup(STDIN_FILENO);
-    *stdout_backup = dup(STDOUT_FILENO);
-    if (*stdin_backup < 0 || *stdout_backup < 0)
-    {
-        perror("Failed to backup file descriptors");
-        return (EXIT_FAILURE);
-    }
-    return (EXIT_SUCCESS);
+	*stdin_backup = dup(STDIN_FILENO);
+	*stdout_backup = dup(STDOUT_FILENO);
+	if (*stdin_backup < 0 || *stdout_backup < 0)
+	{
+		perror("Failed to backup file descriptors");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
-void shell_initialize(t_minishell *minishell)
+void	shell_initialize(t_minishell *minishell)
 {
-    ft_bzero(minishell, sizeof(t_minishell));
-    minishell->envp = initialize_env();
-    minishell->path = get_paths(minishell->envp);
-    if (backup_standard_fds(&minishell->stdin_backup, &minishell->stdout_backup) != EXIT_SUCCESS)
-    {
-        free_all(minishell);
-        exit(EXIT_FAILURE);
-    }
-    initialize_signals();
+	ft_bzero(minishell, sizeof(t_minishell));
+	minishell->envp = initialize_env();
+	minishell->path = get_paths(minishell->envp);
+	if (backup_standard_fds(&minishell->stdin_backup, &minishell->stdout_backup)
+		!= EXIT_SUCCESS)
+	{
+		free_all(minishell);
+		exit(EXIT_FAILURE);
+	}
+	initialize_signals();
 }
--- src/1.initialization/1.main/main.c
+++ src/1.initialization/1.main/main.c
@@ -3,31 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:16:19 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 19:24:23 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:06:22 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/shell.h"
 
-static void cleanup_minishell(t_minishell *minishell)
+static void	cleanup_minishell(t_minishell *minishell)
 {
-    free_all(minishell);
-    if (minishell->stdin_backup >= 0)
-        close(minishell->stdin_backup);
-    if (minishell->stdout_backup >= 0)
-        close(minishell->stdout_backup);
+	free_all(minishell);
+	if (minishell->stdin_backup >= 0)
+		close(minishell->stdin_backup);
+	if (minishell->stdout_backup >= 0)
+		close(minishell->stdout_backup);
 }
 
-int main(void)
+int	main(void)
 {
-    t_minishell minishell;
-    
-    shell_initialize(&minishell);
-    shell_loop(&minishell);
-    cleanup_minishell(&minishell);
+	t_minishell	minishell;
 
-    return EXIT_SUCCESS;
+	shell_initialize(&minishell);
+	shell_loop(&minishell);
+	cleanup_minishell(&minishell);
+	return (EXIT_SUCCESS);
 }
