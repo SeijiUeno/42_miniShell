f026cd5fd9379dcf1521349a8ace09fca5e2fe36
nardini <evandro.nardini@gmail.com>
2024-12-13 14:21:06 -0300

    env patch 1

--- src/1.initialization/0.env/get_env.c
+++ src/1.initialization/0.env/get_env.c
@@ -3,51 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   get_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 18:53:41 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 19:00:55 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:14:22 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/shell.h"
 
-static char **allocate_environment_array(char **environment, int *count)
+static char	**allocate_environment_array(char **environment, int *count)
 {
-    char **new_environment;
+	char	**new_environment;
 
-    *count = 0;
-    while (environment[*count])
-        (*count)++;
-
-    new_environment = ft_calloc(*count + 1, sizeof(char *));
-    return (new_environment);
+	*count = 0;
+	while (environment[*count])
+		(*count)++;
+	new_environment = ft_calloc(*count + 1, sizeof(char *));
+	return (new_environment);
 }
 
-static char **free_environment_array(char **environment, int index)
+static char	**free_environment_array(char **environment, int index)
 {
-    while (--index >= 0)
-        free(environment[index]);
-    free(environment);
-    return (NULL);
+	while (--index >= 0)
+		free(environment[index]);
+	free(environment);
+	return (NULL);
 }
 
-char **get_env(char **environment)
+char	**get_env(char **environment)
 {
-    char **new_environment;
-    int environment_count;
-    int index;
+	char	**new_environment;
+	int		environment_count;
+	int		index;
 
-    new_environment = allocate_environment_array(environment, &environment_count);
-    if (!new_environment)
-        return (NULL);
-
-    for (index = 0; index < environment_count; index++)
-    {
-        new_environment[index] = ft_strdup(environment[index]);
-        if (!new_environment[index])
-            return free_environment_array(new_environment, index);
-    }
-    new_environment[environment_count] = (NULL);
-    return (new_environment);
+	new_environment = allocate_environment_array(environment, &environment_count);
+	if (!new_environment)
+		return (NULL);
+	for (index = 0; index < environment_count; index++)
+	{
+		new_environment[index] = ft_strdup(environment[index]);
+		if (!new_environment[index])
+			return (free_environment_array(new_environment, index));
+	}
+	new_environment[environment_count] = (NULL);
+	return (new_environment);
 }
