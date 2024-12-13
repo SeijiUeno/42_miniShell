baeaddcc66cd2f44ac1dd74e0067cc99c25af95e
nardini <evandro.nardini@gmail.com>
2024-12-13 18:48:40 -0300

    patch buildins

--- src/2.buildins/cd.c
+++ src/2.buildins/cd.c
@@ -3,99 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:38:03 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 19:45:31 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/13 16:13:45 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/shell.h"
 
-static int is_current_directory(const char *path)
+static int	is_current_directory(const char *path)
 {
-    return (*path == '\0' || !ft_strncmp(path, ".", ft_strlen(".") + 1));
+	return (*path == '\0' || !ft_strncmp(path, ".", ft_strlen(".") + 1));
 }
 
-static int print_cd_error(const char *path, const char *error_msg)
+static int	print_cd_error(const char *path, const char *error_msg)
 {
-    if (error_msg)
-    {
-        ft_putstr_fd("cd: ", STDERR_FILENO);
-        ft_putstr_fd(error_msg, STDERR_FILENO);
-        ft_putstr_fd("\n", STDERR_FILENO);
-    }
-    else if (path) // Ensure `path` is not NULL
-    {
-        ft_putstr_fd("cd: ", STDERR_FILENO);
-        ft_putstr_fd(path, STDERR_FILENO);
-        ft_putstr_fd(": ", STDERR_FILENO);
-        ft_putstr_fd(strerror(errno), STDERR_FILENO);
-        ft_putstr_fd("\n", STDERR_FILENO);
-    }
-    return (EXIT_FAILURE);
+	if (error_msg)
+	{
+		ft_putstr_fd("cd: ", STDERR_FILENO);
+		ft_putstr_fd(error_msg, STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+	}
+	else if (path) // Ensure `path` is not NULL
+	{
+		ft_putstr_fd("cd: ", STDERR_FILENO);
+		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+		ft_putstr_fd(strerror(errno), STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+	}
+	return (EXIT_FAILURE);
 }
 
-void update_pwd(t_minishell *minishell)
+void	update_pwd(t_minishell *minishell)
 {
-    char *current_path = getcwd(NULL, 0);
-    char *old_pwd = NULL;
-    char **envp = minishell->envp;
-    int i = 0;
+	char	*current_path;
+	char	*old_pwd;
+	char	**envp;
+	int		i;
+	char	*old_pwd_var;
 
-    while (envp[i])
-    {
-        if (!ft_strncmp(envp[i], "PWD=", 4))
-        {
-            old_pwd = ft_strdup(envp[i]);
-            break;
-        }
-        i++;
-    }
-
-    if (old_pwd)
-    {
-        char *old_pwd_var = ft_strjoin("OLD", old_pwd);
-        free(envp[i]);
-        envp[i] = ft_strjoin("PWD=", current_path);
-        buildin_export((char *[]){ "buildin_export", old_pwd_var, NULL }, minishell);
-        free(old_pwd_var);
-        free(old_pwd);
-    }
-    free(current_path);
+	current_path = getcwd(NULL, 0);
+	old_pwd = NULL;
+	envp = minishell->envp;
+	i = 0;
+	while (envp[i])
+	{
+		if (!ft_strncmp(envp[i], "PWD=", 4))
+		{
+			old_pwd = ft_strdup(envp[i]);
+			break ;
+		}
+		i++;
+	}
+	if (old_pwd)
+	{
+		old_pwd_var = ft_strjoin("OLD", old_pwd);
+		free(envp[i]);
+		envp[i] = ft_strjoin("PWD=", current_path);
+		buildin_export((char *[]){"buildin_export", old_pwd_var, NULL},
+			minishell);
+		free(old_pwd_var);
+		free(old_pwd);
+	}
+	free(current_path);
 }
 
-static char *resolve_home_directory(const char *path, const char *home)
+static char	*resolve_home_directory(const char *path, const char *home)
 {
-    if (!path || !ft_strncmp(path, "~", ft_strlen("~") + 1))
-        return (ft_strdup(home));
-    if (!ft_strncmp(path, "~/", 2))
-        return (ft_strjoin(home, path + 1));
-    return (ft_strdup(path));
+	if (!path || !ft_strncmp(path, "~", ft_strlen("~") + 1))
+		return (ft_strdup(home));
+	if (!ft_strncmp(path, "~/", 2))
+		return (ft_strjoin(home, path + 1));
+	return (ft_strdup(path));
 }
 
-int verify_and_change_dir(const char *path, t_minishell *minishell)
+int	verify_and_change_dir(const char *path, t_minishell *minishell)
 {
-    char *resolved_path;
-    const char *home = getenv("HOME");
+	char		*resolved_path;
+	const char	*home = getenv("HOME");
 
-    resolved_path = resolve_home_directory(path, home);
-    if (chdir(resolved_path) == -1)
-    {
-        free(resolved_path);
-        return (print_cd_error(path, NULL));
-    }
-
-    update_pwd(minishell);
-    free(resolved_path);
-    return (EXIT_SUCCESS);
+	resolved_path = resolve_home_directory(path, home);
+	if (chdir(resolved_path) == -1)
+	{
+		free(resolved_path);
+		return (print_cd_error(path, NULL));
+	}
+	update_pwd(minishell);
+	free(resolved_path);
+	return (EXIT_SUCCESS);
 }
 
-int change_dir(char **args, t_minishell *minishell)
+int	change_dir(char **args, t_minishell *minishell)
 {
-    if (args[1] && args[2])
-        return (print_cd_error(NULL, "too many arguments"));
-    if (args[1] && is_current_directory(args[1]))
-        return (EXIT_SUCCESS);
-    return (verify_and_change_dir(args[1], minishell));
+	if (args[1] && args[2])
+		return (print_cd_error(NULL, "too many arguments"));
+	if (args[1] && is_current_directory(args[1]))
+		return (EXIT_SUCCESS);
+	return (verify_and_change_dir(args[1], minishell));
 }
--- src/2.buildins/exit.c
+++ src/2.buildins/exit.c
@@ -3,82 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 17:14:10 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 19:32:25 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/13 18:37:26 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/shell.h"
 
-static int ft_is_number(const char *str)
+static int	ft_is_number(const char *str)
 {
-    int i;
+	int	i;
 
-    if (!str || !*str)
-        return (0); // Empty string is not a number
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    if (!str[i])
-        return (0); // String is just "+" or "-"
-    while (str[i])
-    {
-        if (!ft_isdigit(str[i]))
-            return (0);
-        i++;
-    }
-    return (1);
+	if (!str || !*str)
+		return (0); // Empty string is not a number
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0); // String is just "+" or "-"
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-static int calculate_exit_code(const char *arg)
+static int	calculate_exit_code(const char *arg)
 {
-    int number;
+	int	number;
 
-    number = ft_atoi(arg);
-    if (number < 0 || number > 255)
-        return (number % 256);
-    return (number);
+	number = ft_atoi(arg);
+	if (number < 0 || number > 255)
+		return (number % 256);
+	return (number);
 }
 
-static void set_exit_code(char **args, t_minishell *minishell)
+static void	set_exit_code(char **args, t_minishell *minishell)
 {
-    int exit_code;
+	int	exit_code;
 
-    if (!args[1])
-        exit_code = minishell->status;
-    else if (ft_is_number(args[1]))
-        exit_code = calculate_exit_code(args[1]);
-    else
-    {
-        ft_putstr_fd("ERROR: exit: ", STDERR_FILENO);
-        ft_putstr_fd(args[1], STDERR_FILENO);
-        ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-        exit_code = 2;
-    }
-
-    free_all(minishell);
-    exit(exit_code);
+	if (!args[1])
+		exit_code = minishell->status;
+	else if (ft_is_number(args[1]))
+		exit_code = calculate_exit_code(args[1]);
+	else
+	{
+		ft_putstr_fd("ERROR: exit: ", STDERR_FILENO);
+		ft_putstr_fd(args[1], STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+		exit_code = 2;
+	}
+	free_all(minishell);
+	exit(exit_code);
 }
 
-int builtin_exit(char **args, t_minishell *minishell)
+int	builtin_exit(char **args, t_minishell *minishell)
 {
-    int arg_count;
+	int	arg_count;
 
-    ft_putstr_fd("exit\n", STDERR_FILENO);
-
-    // Count the number of arguments
-    arg_count = 0;
-    while (args[arg_count])
-        arg_count++;
-
-    if (arg_count > 2 && ft_is_number(args[1]))
-    {
-        ft_putstr_fd("ERROR: exit: too many arguments\n", STDERR_FILENO);
-        return (EXIT_FAILURE);
-    }
-
-    set_exit_code(args, minishell);
-    return (EXIT_SUCCESS); // This won't be reached due to exit()
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	// Count the number of arguments
+	arg_count = 0;
+	while (args[arg_count])
+		arg_count++;
+	if (arg_count > 2 && ft_is_number(args[1]))
+	{
+		ft_putstr_fd("ERROR: exit: too many arguments\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	set_exit_code(args, minishell);
+	return (EXIT_SUCCESS); // This won't be reached due to exit()
 }
