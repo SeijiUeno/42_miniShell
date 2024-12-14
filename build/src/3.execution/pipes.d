60f1c12834ee43d87a568d5b994b9c482387d306
Seiji Ueno <142241481+SeijiUeno@users.noreply.github.com>
2024-12-14 15:55:35 -0300

    Return fix

--- build/src/3.execution/pipes.d
+++
@@ -1,9 +0,0 @@
-build/src/3.execution/pipes.o: src/3.execution/pipes.c \
-  includes/../includes/shell.h includes/../libs/libft/includes/libft.h \
-  includes/../libs/garbage_collector/includes/garbage_collector.h
-
-includes/../includes/shell.h:
-
-includes/../libs/libft/includes/libft.h:
-
-includes/../libs/garbage_collector/includes/garbage_collector.h:
--- build/src/aux/free.d
+++
@@ -1,9 +0,0 @@
-build/src/aux/free.o: src/aux/free.c includes/../includes/shell.h \
-  includes/../libs/libft/includes/libft.h \
-  includes/../libs/garbage_collector/includes/garbage_collector.h
-
-includes/../includes/shell.h:
-
-includes/../libs/libft/includes/libft.h:
-
-includes/../libs/garbage_collector/includes/garbage_collector.h:
