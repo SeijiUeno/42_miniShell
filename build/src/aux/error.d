fa87ad0b58fabb1faa89923d4070c2c007298f7f
Seiji Ueno <142241481+SeijiUeno@users.noreply.github.com>
2024-12-14 15:30:07 -0300

    status update

--- build/src/aux/error.d
+++
@@ -1,9 +0,0 @@
-build/src/aux/error.o: src/aux/error.c includes/../includes/shell.h \
-  includes/../libs/libft/includes/libft.h \
-  includes/../libs/garbage_collector/includes/garbage_collector.h
-
-includes/../includes/shell.h:
-
-includes/../libs/libft/includes/libft.h:
-
-includes/../libs/garbage_collector/includes/garbage_collector.h:
