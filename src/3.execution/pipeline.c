2397bd97bda007f6c3c053be3b8fc519fb6a6dd6
nardini <evandro.nardini@gmail.com>
2024-12-14 19:29:15 -0300

    partial exec norminette

--- src/3.execution/pipeline.c
+++ src/3.execution/pipeline.c
@@ -3,55 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   pipeline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:25:42 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/14 17:35:55 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/14 19:18:37 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/shell.h"
 
-int run_single_command(t_minishell *m, t_command *c) {
-    if (c->redir && redir_setup(c->redir) != 0)
-        return (EXIT_FAILURE);
-    if (!c->argv)
-        return (0);
-    if (builtin_check(c->argv, m) >= 0)
-        return (m->status);
-    return (exec_run_command(c->argv, 0, m));
+int	run_single_command(t_minishell *m, t_command *c)
+{
+	if (c->redir && redir_setup(c->redir) != 0)
+		return (EXIT_FAILURE);
+	if (!c->argv)
+		return (0);
+	if (builtin_check(c->argv, m) >= 0)
+		return (m->status);
+	return (exec_run_command(c->argv, 0, m));
 }
 
-void fork_children(t_minishell *m, t_command **cmds, int cmd_count, int *pipes) {
-    pid_t *pids = ft_calloc(sizeof(pid_t), cmd_count);
+void	fork_children(t_minishell *m, t_command **cmds,
+	int cmd_count, int *pipes)
+{
+	pid_t	*pids = ft_calloc(sizeof(pid_t), cmd_count);
 
-    for (int i = 0; i < cmd_count; i++) {
-        if ((pids[i] = fork()) == 0) {
-            int ctx[] = {i, cmd_count};
-            exec_run_child_command(m, cmds[i], pipes, ctx);
-        }
-    }
-    pipe_close_all(pipes, (cmd_count - 1) * 2);
-    exec_wait_for_children(m, pids, cmd_count);
-    free(pids);
+	for (int i = 0; i < cmd_count; i++)
+	{
+		if ((pids[i] = fork()) == 0)
+		{
+			int	ctx[] = {i, cmd_count};
+			exec_run_child_command(m, cmds[i], pipes, ctx);
+		}
+	}
+	pipe_close_all(pipes, (cmd_count - 1) * 2);
+	exec_wait_for_children(m, pids, cmd_count);
+	free(pids);
 }
 
-void pipe_run_pipeline(t_minishell *m, t_command **cmds, int cmd_count) {
-    if (cmd_count == 1) {
-        g_in_subprocess = 1;
-        m->status = run_single_command(m, cmds[0]);
-        g_in_subprocess = 0;
-        return;
-    }
-    int *pipes = ft_calloc(sizeof(int), (cmd_count - 1) * 2);
-    if (!pipes)
-        return;
-    if (pipe_create(pipes, cmd_count) != EXIT_SUCCESS) {
-        free(pipes);
-        return;
-    }
-    g_in_subprocess = 1;
-    fork_children(m, cmds, cmd_count, pipes);
-    g_in_subprocess = 0;
-    free(pipes);
+void	pipe_run_pipeline(t_minishell *m, t_command **cmds, int cmd_count)
+{
+	if (cmd_count == 1)
+	{
+		g_in_subprocess = 1;
+		m->status = run_single_command(m, cmds[0]);
+		g_in_subprocess = 0;
+		return ;
+	}
+	int	*pipes = ft_calloc(sizeof(int), (cmd_count - 1) * 2);
+	if (!pipes)
+		return ;
+	if (pipe_create(pipes, cmd_count) != EXIT_SUCCESS)
+	{
+		free(pipes);
+		return ;
+	}
+	g_in_subprocess = 1;
+	fork_children(m, cmds, cmd_count, pipes);
+	g_in_subprocess = 0;
+	free(pipes);
 }
--- src/3.execution/pipes.c
+++ src/3.execution/pipes.c
@@ -3,28 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   pipes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: emorales <emorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:18:11 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/12 19:40:11 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/14 19:23:40 by emorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/shell.h"
 
-void pipe_close_all(int *pipes, int total_fds) {
-    for (int i = 0; i < total_fds; i++)
-        close(pipes[i]);
+void	pipe_close_all(int *pipes, int total_fds)
+{
+	for (int i = 0; i < total_fds; i++)
+		close(pipes[i]);
 }
 
-int pipe_create(int *pipes, int command_count) {
-    int total_fds = (command_count - 1) * 2;
-    for (int i = 0; i < total_fds; i += 2) {
-        if (pipe(&pipes[i]) < 0) {
-            perror("pipe");
-            pipe_close_all(pipes, i);
-            return (EXIT_FAILURE);
-        }
-    }
-    return (EXIT_SUCCESS);
+int	pipe_create(int *pipes, int command_count)
+{
+	int	total_fds;
+
+	total_fds = (command_count - 1) * 2;
+	for (int i = 0; i < total_fds; i += 2)
+	{
+		if (pipe(&pipes[i]) < 0)
+		{
+			perror("pipe");
+			pipe_close_all(pipes, i);
+			return (EXIT_FAILURE);
+		}
+	}
+	return (EXIT_SUCCESS);
 }
