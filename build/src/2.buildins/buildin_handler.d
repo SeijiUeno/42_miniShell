080bcf78568048be33e74bd299952b0ce7902d5b
Seiji Ueno <sueno-te@c2r12p2.42sp.org.br>
2024-12-14 15:55:35 -0300

    Return fix

--- build/src/2.buildins/buildin_handler.d
+++
@@ -1,9 +0,0 @@
-build/src/2.buildins/buildin_handler.o: src/2.buildins/buildin_handler.c \
-  includes/../includes/shell.h includes/../libs/libft/includes/libft.h \
-  includes/../libs/garbage_collector/includes/garbage_collector.h
-
-includes/../includes/shell.h:
-
-includes/../libs/libft/includes/libft.h:
-
-includes/../libs/garbage_collector/includes/garbage_collector.h:
