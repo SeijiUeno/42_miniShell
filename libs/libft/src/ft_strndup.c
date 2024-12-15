1b0f2f578583a1fec63fa2cbba39d538b51aec7b
Seiji Ueno <sueno-te@c1r5p1.42sp.org.br>
2024-12-15 05:34:27 -0300

    libs

--- libs/libft/src/ft_strndup.c
+++ libs/libft/src/ft_strndup.c
@@ -12,27 +12,26 @@
 
 #include "../includes/libft.h"
 
-char *ft_strndup(const char *s, size_t n)
+char	*ft_strndup(const char *s, size_t n)
 {
-    size_t len;
-    char *copy;
-    size_t i;
+	size_t	len;
+	char	*copy;
+	size_t	i;
 
-    len = 0;
-    if (!s)
-        return NULL;
-    while (s[len] && len < n)
-        len++;
-    copy = (char *)malloc(len + 1); 
-    if (!copy)
-        return NULL;
-    i = 0;
-    while (i < len)
-    {
-        copy[i] = s[i];
-        i++;
-    }
-    copy[len] = '\0';
-
-    return copy;
+	len = 0;
+	if (!s)
+		return (NULL);
+	while (s[len] && len < n)
+		len++;
+	copy = (char *)malloc(len + 1);
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[len] = '\0';
+	return (copy);
 }
