b5849f85169e126f1e0d9cd678ba8954218a8762
Seiji Ueno <142241481+SeijiUeno@users.noreply.github.com>
2024-12-15 06:01:28 -0300

    Revert "ast ok"
    This reverts commit b56ec377e8d8f2cedbd7914ffbb433918c6c3b74.

--- src/1.initialization/4.ast/ast_tokens.c
+++ src/1.initialization/4.ast/ast_tokens.c
@@ -6,12 +6,17 @@
 /*   By: sueno-te <sueno-te@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 14:45:08 by sueno-te          #+#    #+#             */
-/*   Updated: 2024/12/15 05:57:22 by sueno-te         ###   ########.fr       */
+/*   Updated: 2024/12/15 05:21:23 by sueno-te         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/shell.h"
 
+/**
+ * Counts the number of arguments in a command up to a pipe.
+ * @param tokens - The list of tokens.
+ * @return Number of arguments before a PIPE token.
+ */
 static int	count_command_arguments(t_token *tokens)
 {
 	int	arg_count;
@@ -26,6 +31,12 @@ static int	count_command_arguments(t_token *tokens)
 	return (arg_count);
 }
 
+/**
+ * Builds the argument vector from tokens.
+ * @param tokens - The token list.
+ * @param minishell - The shell context.
+ * @return The argument vector or NULL on failure.
+ */
 char	**build_argument_vector(t_token *tokens, t_ms *minishell)
 {
 	int	count;
@@ -36,6 +47,11 @@ char	**build_argument_vector(t_token *tokens, t_ms *minishell)
 	return (allocate_argument_array(tokens, minishell, count));
 }
 
+/**
+ * Frees partially allocated argument array in case of error.
+ * @param argv - The argument array.
+ * @param used - Number of arguments allocated.
+ */
 static void	free_argument_array_partially(char **argv, int used)
 {
 	while (used--)
@@ -43,9 +59,15 @@ static void	free_argument_array_partially(char **argv, int used)
 	free(argv);
 }
 
+/**
+ * Allocates and populates the argument array with expanded variables and quotes.
+ * @param tokens - The token list.
+ * @param minishell - The shell context.
+ * @param count - The number of arguments to allocate.
+ * @return The populated argument array or NULL on failure.
+ */
 char	**allocate_argument_array(t_token *tokens,
-								t_ms *minishell,
-								int count)
+	t_ms *minishell, int count)
 {
 	char	**argv;
 	int		index;
@@ -72,6 +94,10 @@ char	**allocate_argument_array(t_token *tokens,
 	return (argv);
 }
 
+/**
+ * Advances the token pointer to the next pipe or end of the list.
+ * @param tokens - A pointer to the token pointer.
+ */
 void	advance_to_next_pipe(t_token **tokens)
 {
 	while (*tokens && (*tokens)->type != PIPE)
